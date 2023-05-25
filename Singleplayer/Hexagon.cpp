dcec9c027894f626d76132b3eecda32f123eb1b8
radekaadek <radekaadek@gmail.com>
2023-05-25 23:15:00 +0200

    del

--- Singleplayer/Hexagon.cpp
+++
@@ -1,7 +0,0 @@
-#include "Hexagon.h"
-#include <cmath>
-
-float myHexagon::distance(const myHexagon* a, const myHexagon* b)
-{
-    return sqrt(pow(a->getPosition().x - b->getPosition().x, 2) + pow(a->getPosition().y - b->getPosition().y, 2));
-}
