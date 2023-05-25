3801f369e8914a35188fecf9f8116b52cb387009
radekaadek <radekaadek@gmail.com>
2023-05-25 22:58:59 +0200

    dasd

--- Multiplayer/Hexagon.cpp
+++
@@ -1,6 +0,0 @@
-#include "Hexagon.h"
-
-float distance (const myHexagon* a, const myHexagon* b)
-{
-    return sqrt(pow(a->getPosition().x - b->getPosition().x, 2) + pow(a->getPosition().y - b->getPosition().y, 2));
-}
